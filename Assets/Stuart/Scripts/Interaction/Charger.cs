4322b230ee92f3fdb4b48decaa4b2152fd1b2afb
PkplonkerGIT <pkplonker@gmail.com>
2024-04-06 13:40:16 +0100

    SH:Battery Charger

---
+++ Assets/Stuart/Scripts/Interaction/Charger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Charger : MonoBehaviour, IHitTarget
+{
+	private Coroutine cor;
+
+	[SerializeField]
+	private float chargeFrequency = 0.2f;
+
+	[SerializeField]
+	private float chargeAmount = 0.2f;
+
+	public static event Action ChargeEnded;
+	private WaitForSeconds wait;
+
+	private void Start()
+	{
+		wait = new WaitForSeconds(chargeFrequency);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		var battery = other.gameObject.GetComponent<Battery>();
+		StopCor();
+		StartCor(battery);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		StopCor();
+	}
+
+	private void StartCor(Battery battery)
+	{
+		cor = StartCoroutine(Charge(battery));
+	}
+
+	private void StopCor()
+	{
+		if (cor != null)
+		{
+			StopCoroutine(cor);
+			cor = null;
+		}
+	}
+
+	private IEnumerator Charge(Battery battery)
+	{
+		while (true)
+		{
+			if (battery.BatteryLevel == battery.batteryStats.MaxBattery)
+			{
+				break;
+			}
+
+			battery.Charge(chargeAmount);
+			yield return wait;
+		}
+
+		ChargeEnded?.Invoke();
+	}
+}
