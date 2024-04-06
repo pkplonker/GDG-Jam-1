d1e0fbef756ce119327116b5117d52f5dabad4a4
PkplonkerGIT <pkplonker@gmail.com>
2024-04-06 22:39:27 +0100

    SH:Simple pause controller

---
+++ Assets/Stuart/Scripts/PauseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class PauseController : GenericUnitySingleton<PauseController>
+{
+	public event Action<bool> PauseChanged;
+
+	private bool isPaused;
+
+	public bool IsPaused
+	{
+		get => isPaused;
+		set
+		{
+			if (isPaused != value)
+			{
+				isPaused = value;
+				PauseChanged?.Invoke(isPaused);
+			}
+		}
+	}
+}
