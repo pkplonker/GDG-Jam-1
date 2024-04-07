72acf6f0f8fe6fa76bdd2468c4d734d855bbd03e
Luke Owen <luke.owen96@hotmail.com>
2024-04-07 13:38:38 +0100

    Basic functionality for player overlay. Added copy of main scene for testing

--- Assets/Luke/Scripts/UI/Overlays/PlayerOverlay.cs
+++ Assets/Luke/Scripts/UI/Overlays/PlayerOverlay.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerOverlay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider battery;
+    public Image chargingImage;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI rubbishCounter;
+
+    private float prevVal;
+
+    private void Awake()
     {
-        
+        prevVal = 1;
+        chargingImage.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnFinishLevel()
     {
+        Debug.Log("Finishing Level");
+    }
+    private void OnEnable()
+    {
+        PlayerStateMachine.Instance.GetComponent<Battery>().BatteryLevelChanged += OnBatteryChanged;
+        RubbishCollectionController.OnScoreChanged += ScoreChanged;
+    }
+
+    private void ScoreChanged(int score)
+    {
+        rubbishCounter.text = score.ToString();
+    }
+
+    private void OnBatteryChanged(float _, float percent)
+    {
+        var mainPercent = percent / 100;
+        if (mainPercent > 1) mainPercent = 1;
+        else if (mainPercent < 0) mainPercent = 0;
         
+        chargingImage.gameObject.SetActive(mainPercent > prevVal && mainPercent!=1);
+
+        battery.SetValueWithoutNotify(mainPercent);
+        prevVal = mainPercent;
     }
 }
