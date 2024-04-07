121ce5a2e00ca4d6bb3cb1434d62e0f2520f4e61
Mia Mohac (mohacm) <miamohac.official@gmail.com>
2024-04-07 03:04:21 +0100

    Cleaning Trace Shader (Hindered Progress)
    Managed to "fix" the shader and how it responds to moving roomba. For now there are placeholder brick and wood texture for floors to test the functionality between different maps. TextureSetter.cs script is the one that fetches the live texture update from the [SYSTEM] Ground Texture Generator.
    
    Sad post-mortem: After pulling 13 changes from github, the little testee roomba doesn't want to move and shader is not displayed on the floor. FML.
    
    Next up:
    1. apply appropriate textures for floors.
    2. Import the floor into the main game
    
    Important note about the code:
    TextureSetter.cs is still janky and doesn't properly fetch the Texture2D "MaskCleaner" within the "Shader Graph Floor". It doesn't go into the conditionals even though trying to get the texture outputs null. It is broken, but doesn't produce errors (??). Something to watch out in case shader is broken again.
    
    Note about the shader:
    There was a need to rotate the mask map for 180 degrees to follow the roomba. Otherwise it would go in opposite direction. This is a quick bug fix, and in case shader doesn't respond as intended look into the shader to remove the UV rotation node.

--- Assets/Mia/Mia's Environment/_Dev Environment/TextureSetter.cs
+++ Assets/Mia/Mia's Environment/_Dev Environment/TextureSetter.cs
@@ -7,35 +7,69 @@ public class TextureSetter : MonoBehaviour
     public GameObject GroundTextureMask;
     public RuntimeGroundTexture RuntimeGroundTextureScript;
     public GroundTextureGenerator GroundTextureGeneratorScript;
-    
+
+    public Material shaderGraphMaterial;
+    private Renderer renderer;
+    public string texturePropertyName = "_MaskCleaner";
+    public MaterialPropertyBlock propertyBlock;
+
     public Texture tex2Fetch;  //Texture that is in the scene
     public Texture tex2Set; //Texture2D that is in the ShaderGraph
-    public Vector2Int textureSize { get; set; } = new Vector2Int(1024, 1024);
-
-    public Material shaderGraphMaterial; 
 
     void Start()
     {
+        //Fetching the texture from the scene
         GroundTextureGeneratorScript = GroundTextureMask.GetComponent<GroundTextureGenerator>();
         tex2Fetch = GroundTextureGeneratorScript.savedTexture;
-        tex2Set = tex2Fetch;
-
-        Renderer renderer = GetComponent<Renderer>();
-        if(renderer != null)
+        if (tex2Fetch == null)
         {
-            shaderGraphMaterial = renderer.material;
-            if(shaderGraphMaterial == null)
-            {
-                Debug.LogWarning("Shader Graph Material not found on GameObject: " + gameObject.name);
-            }
-            else
-            {
-                Debug.LogWarning("Shader Graph Material is found: " + shaderGraphMaterial.name);
-            }
+            Debug.LogWarning("Texture to fetch component not found on GameObject: " + gameObject.name);
         }
         else
         {
+            //Debug.Log("Fetched Texture from Scene: " + tex2Fetch.name);
+
+            //Changing the texture
+            //propertyBlock.SetTexture(texturePropertyName, tex2Fetch);
+            //renderer.SetPropertyBlock(propertyBlock);
+
+            //shaderGraphMaterial.SetTexture(texturePropertyName, tex2Fetch);
+        }
+        //tex2Set = tex2Fetch;
+
+        renderer = GetComponent<Renderer>();         
+        if (renderer == null)
+        {
             Debug.LogWarning("Renderer component not found on GameObject: " + gameObject.name);
+            return;
+        }
+
+        shaderGraphMaterial = renderer.material;
+        if (shaderGraphMaterial == null)
+        {
+            Debug.LogWarning("Shader Graph Material not found on GameObject: " + gameObject.name);
+        }
+        //Debug.Log("Shader Graph Material is " + renderer.material);
+
+        propertyBlock = new MaterialPropertyBlock();
+        renderer.GetPropertyBlock(propertyBlock);
+        var texture = propertyBlock.GetTexture(texturePropertyName);
+        tex2Set = texture;
+        
+        texturePropertyName = shaderGraphMaterial.GetTexture("_MaskCleaner").name;
+        //tex2Set = renderer.material.GetTexture(texturePropertyName);
+        //texturePropertyID = Shader.PropertyToID("_MaskCleaner");       
+        if (tex2Set != null)
+        {
+            Debug.Log("Fetched Texture2D: " + tex2Set.name);
+            //Debug.Log("Fetched texturePropertyID: " + texturePropertyID);
+                        
+            //Changing the texture
+            propertyBlock.SetTexture(propertyBlock.GetTexture(texturePropertyName).name, tex2Fetch);
+            renderer.SetPropertyBlock(propertyBlock);
+        } else
+        {
+            Debug.LogWarning("Texture with the name '" + texturePropertyName + "' not found in the shader on the GameObject " + gameObject.name);            
         }
     }
 
@@ -43,20 +77,30 @@ public class TextureSetter : MonoBehaviour
     {
         RuntimeGroundTextureScript = GroundTextureMask.GetComponent<RuntimeGroundTexture>();
         tex2Fetch = RuntimeGroundTextureScript.texture;
-        tex2Set = tex2Fetch;
-
-        if (shaderGraphMaterial != null && tex2Set != null)
+        if (tex2Fetch == null)
         {
-            //shaderGraphMaterial.SetTexture("Mask Cleaner", tex2Set);
-            shaderGraphMaterial.mainTexture = tex2Set;
+            Debug.LogWarning("Texture to fetch component not found on GameObject: " + gameObject.name);
+        }
+
+        if (tex2Fetch != null && renderer != null)
+        {
+            //Changing the texture
+            propertyBlock.SetTexture(texturePropertyName, tex2Fetch);
+            renderer.SetPropertyBlock(propertyBlock);
+
+            shaderGraphMaterial.SetTexture(0, tex2Fetch);
+        }
+
+        if (shaderGraphMaterial != null && tex2Fetch != null)
+        {
+            //this one doesn't work
+            //shaderGraphMaterial.SetTexture(texturePropertyName, tex2Fetch);
+            //Debug.Log("Trying to set the texture on a Shader Graph"); //We do get to this part
+            //shaderGraphMaterial.mainTexture = tex2Set;                     
         }
         else if (shaderGraphMaterial == null)
         {
-            Debug.LogWarning("Shader Graph Material not found on GameObject: " + gameObject.name);
-        }
-        else if (tex2Set == null)
-        {
-            Debug.LogWarning("Texture to set component not found on GameObject: " + gameObject.name);
+            //Debug.LogWarning("Shader Graph Material not found on GameObject: " + gameObject.name);
         }
     }
 }
